5d26a0245904b81ca722937cd828021d56708ad0
Jayson <jayson@mac.local>
2018-03-19 01:47:57 +0800

    modify route

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,14 @@
 
 angular.module("voicetube", ["ngRoute"])
-    .config(['$routeProvider', '$locationProvider',
-        function ($routeProvider, $locationProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl: 'view/main/main.html',
-                    controller: 'MainController',
-                    controllerAs: 'vm'
-                })
-            $locationProvider.html5Mode({
-                enabled: true,
-                requireBase: false
-            });
-        }])
+.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'view/main/main.html',
+            controller: 'MainController',
+            controllerAs: 'vm'
+        })
+        .otherwise({
+            template : "<h1>None</h1><p>Nothing has been selected</p>"
+        });
 
-
-
+    }]);
