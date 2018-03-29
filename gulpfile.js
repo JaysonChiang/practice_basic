e6f2ec62b8385fe4261c5e7e561afe927853ebb3
Jayson <jayson@mac.local>
2018-03-29 21:58:18 +0800

    init scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,29 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect');
 
-gulp.task('connect', function() {
-  connect.server({
-    root: 'app',
-    livereload: true
-  });
+gulp.task('connect', function () {
+    connect.server({
+        root: 'app',
+        livereload: true
+    });
+});
+
+gulp.task('scss', function () {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./app/css'))
+        .pipe(connect.reload());
 });
 
 gulp.task('html', function () {
-  gulp.src(['./app/*.html'])
-    .pipe(gulp.dest('./app'))
-    .pipe(connect.reload());
+    gulp.src(['./app/*.html'])
+        .pipe(gulp.dest('./app'))
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./app/*.html','./view/main/'], ['html']);
+    gulp.watch(['./app/*.html', './view/main/','./scss/**/*.scss'], ['html','scss']);
 });
 
 gulp.task('default', ['connect', 'watch']);
